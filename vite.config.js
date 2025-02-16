2cd12f07cc1bc67605e61fd3d2cdafcbec78e4a4
nousantx <nousantx@gmail.com>
2025-02-16 18:33:14 +0700

    feat: cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,5 @@
-// import AnyFrame from './plugin.js'
 import AnyFrame from 'vite-plugin-anyframe'
 
 export default {
-  plugins: [
-    AnyFrame({
-      debug: {
-        enabled: true,
-        showClassNames: true,
-        showFileChanges: true,
-        showStylesheet: true,
-        logPrefix: '[AnyFrame Debug]'
-      }
-    })
-  ]
+  plugins: [AnyFrame()]
 }
